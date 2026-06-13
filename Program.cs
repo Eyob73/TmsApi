1ea7b5a4a3cacfcd94f92e13fc43125e1381e848
Eyob <eyget73@gmail.com>
2026-06-13 13:03:49 +0300

    fix: Middleware Ordering

--- Program.cs
+++ Program.cs
@@ -1,17 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseExceptionHandler("/error");
 
-app.UseHttpsRedirection();
+app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapGet("/api/assessments/results", () => Results.Ok(new
+{
+    courseCode = "CS-101",
+    studentId = "S-001",
+    letterGrade = "A"
+})).RequireAuthorization();
 
 app.Run();

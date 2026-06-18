9aa2f8bdfed99d662845bdedd0a3aba6c5c7ea4d
Eyob <eyget73@gmail.com>
2026-06-18 20:41:21 +0300

    chore(linq): demonstrate query translation, deferred execution, and logging behavior

--- obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+699769e74a04c4f975866b62665bada72cb1f850")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a4bee40b3b2446df3bfa763e39e456d718873e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
